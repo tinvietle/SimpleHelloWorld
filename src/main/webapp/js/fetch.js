b72b80e639b85927c99c843f685378ed33c66e53
tinvietle <tinvietle@gmail.com>
2025-10-21 15:19:39 +0700

    a

--- src/main/webapp/js/fetch.js
+++ src/main/webapp/js/fetch.js
@@ -3,55 +3,57 @@ const BASE_URL = '/demo';
 function fetchObject(key) {
     let url = `${BASE_URL}/object/${key}`;
     fetch(url)
-    .then(response => response.blob())
-    .then((myBlob) => {
-        const objectURL = URL.createObjectURL(myBlob);
-        const img_S3 = document.getElementById('s3-image');
-        img_S3.src = objectURL;
-    });
+        .then(response => response.blob())
+        .then((myBlob) => {
+            const objectURL = URL.createObjectURL(myBlob);
+            const img_S3 = document.getElementById('s3-image');
+            img_S3.src = objectURL;
+        });
 }
 
 function fetchObjectWithRole(key) {
     let url = `${BASE_URL}/object-with-role/${key}`;
     fetch(url)
-    .then(response => response.blob())
-    .then((myBlob) => {
-        const objectURL = URL.createObjectURL(myBlob);
-        const img_S3 = document.getElementById('s3-image');
-        img_S3.src = objectURL;
-    });
+        .then(response => response.blob())
+        .then((myBlob) => {
+            const objectURL = URL.createObjectURL(myBlob);
+            const img_S3 = document.getElementById('s3-image');
+            img_S3.src = objectURL;
+        });
 }
 
 function fetchObjectFromBucketWithRole() {
     let url = `${BASE_URL}/object-bucket-with-role`;
-    const tableBody = document.getElementById('table-body');
-    fetch(url)
-    .then(response => response.text())
-    .then((textData) => {
-        const lines = textData.split('\n');
-        const keys = lines.slice(2).filter(key => key.trim() !== '');
+    document.addEventListener('DOMContentLoaded', () => {
+        const tableBody = document.getElementById('table-body');
+        fetch(url)
+            .then(response => response.text())
+            .then((textData) => {
+                const lines = textData.split('\n');
+                const keys = lines.slice(2).filter(key => key.trim() !== '');
 
-        keys.forEach(key => {
-            const row = document.createElement('tr');
+                keys.forEach(key => {
+                    const row = document.createElement('tr');
 
-            const cellImage = document.createElement('td');
-            const img = document.createElement('img');
-            img.width = 100;
-            img.height = 100;
-            img.src = `${BASE_URL}/object-bucket-with-role/${key}`;
-            cellImage.appendChild(img);
-            row.appendChild(cellImage);
+                    const cellImage = document.createElement('td');
+                    const img = document.createElement('img');
+                    img.width = 100;
+                    img.height = 100;
+                    img.src = `${BASE_URL}/object-bucket-with-role/${key}`;
+                    cellImage.appendChild(img);
+                    row.appendChild(cellImage);
 
-            const cellDownload = document.createElement('td');
-            const downloadLink = document.createElement('a');
-            downloadLink.textContent = 'Download';
-            downloadLink.className = 'download-button';
-            downloadLink.href = `${BASE_URL}/object-bucket-with-role/${key}`;
-            cellDownload.appendChild(downloadLink);
+                    const cellDownload = document.createElement('td');
+                    const downloadLink = document.createElement('a');
+                    downloadLink.textContent = 'Download';
+                    downloadLink.className = 'download-button';
+                    downloadLink.href = `${BASE_URL}/object-bucket-with-role/${key}`;
+                    cellDownload.appendChild(downloadLink);
 
-            row.appendChild(cellDownload);
+                    row.appendChild(cellDownload);
 
-            tableBody.appendChild(row);
-        });
+                    tableBody.appendChild(row);
+                });
+            });
     });
 }
